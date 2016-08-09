acfefa9105a029193e2d4f6e35c37c5551a446dc
Ryan Vinyard <Guest@epicodus-44.local>
2016-08-09 16:05:35 -0700

    add pretty looking flippable cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".col-md-3").click(function() {
-  $(this).not(".hide").children().fadeToggle();
-  $()
+  $(this).slideToggle().stop();
+  $(this).children().slideToggle();
   });
 });
